ef4da64ee0ae12d156eea89939d5df541c46effe
Thiago <tfrancar@gmail.com>
2023-09-10 17:47:37 -0300

    Criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+
+	fmt.Println(ContaCorrente{})
+}
