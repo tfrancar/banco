14af389a0c26987ae82f058d0119c2ec4e5f2094
Thiago <tfrancar@gmail.com>
2023-09-10 17:57:34 -0300

    Usando as três forma de declação de uso de uma strutc

--- main.go
+++ main.go
@@ -10,6 +10,28 @@ type ContaCorrente struct {
 }
 
 func main() {
+	contaDoThiago := ContaCorrente{
+		titular:       "Thiago",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.62,
+	}
 
-	fmt.Println(ContaCorrente{})
+	// Quando vamos popular todos os campos da estrura podemos usar este segundo modo.
+	contaDaKelma := ContaCorrente{
+		"Kelma",
+		589,
+		1234567,
+		1125.62,
+	}
+
+	// Quando vamos popular campos especificos.
+	contaDoGuilherme := ContaCorrente{
+		titular: "Guilherme",
+		saldo:   1125.62,
+	}
+
+	fmt.Println(contaDoThiago)
+	fmt.Println(contaDaKelma)
+	fmt.Println(contaDoGuilherme)
 }
